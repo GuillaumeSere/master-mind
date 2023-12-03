cb9cfcfd0ec89732eac1c081077168d9abaef608
Guillaume Sere <guillaumesere@hotmail.fr>
2023-12-03 16:34:50 +0100

    add QR Code

--- src/components/mastermind.js
+++ src/components/mastermind.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Rules } from './rules';
-import { Game } from './game'
+import { Game } from './game';
+import qr from "./images/QR.jpg";
 import '../App.css';
 
 export class Mastermind extends React.Component {
     render() {
       return (
       <div className='container'>
+        <span class="span2">QR CODE Du Site</span>
+        <img src={qr} alt="" />
         <h2 className='title'> MasterMind </h2>
         <Rules />
         <Game />      
